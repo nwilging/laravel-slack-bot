16e4a919f6485a00d010fc6b30813286ffb099a0
Nick Wilging <nick@epautomotive.org>
2022-04-24 13:35:21 -0400

    adds SlackApiNotificationContract to be implemented by any notification class that will support this package adds configurable driver name so this package can be used alongside laravel/slack-notification-channel update tests update readme and examples

--- config/slack.php
+++ config/slack.php
@@ -4,4 +4,5 @@ declare(strict_types=1);
 return [
     'bot_token' => env('SLACK_API_BOT_TOKEN'),
     'api_url' => env('SLACK_API_URL', 'https://slack.com/api'),
+    'driver_name' => env('SLACK_API_DRIVER_NAME', 'slack'),
 ];
--- src/Contracts/Channels/SlackNotificationChannelContract.php
+++ src/Contracts/Channels/SlackNotificationChannelContract.php
@@ -3,9 +3,18 @@ declare(strict_types=1);
 
 namespace Nwilging\LaravelSlackBot\Contracts\Channels;
 
-use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Notifiable;
+use Nwilging\LaravelSlackBot\Contracts\Notifications\SlackApiNotificationContract;
 
 interface SlackNotificationChannelContract
 {
-    public function send($notifiable, Notification $notification): void;
+    /**
+     * Send a notification via the Slack API. The `$notification` should be an implementation of
+     * the SlackApiNotificationContract.
+     *
+     * @param Notifiable $notifiable
+     * @param SlackApiNotificationContract $notification
+     * @return void
+     */
+    public function send($notifiable, SlackApiNotificationContract $notification): void;
 }
